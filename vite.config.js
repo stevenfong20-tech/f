ad5fe787cde1ab09bc29ad764a38b83040a54e6f
stevenfong20-tech <stevenfong20@gmail.com>
2026-06-03 13:28:22 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import base44 from "@base44/vite-plugin"
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import { fileURLToPath, URL } from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -17,10 +16,5 @@ export default defineConfig({
       visualEditAgent: true
     }),
     react(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+  ]
 });
